7abbf4b94ed7fd4b25493a4154fcf4aadc28bfd4
Graeme Coupar <grambo@grambo.me.uk>
2015-07-18 15:50:07 +0100

    Add code for PlugFprof.

--- lib/plug_fprof.ex
+++ lib/plug_fprof.ex
@@ -1,2 +1,23 @@
 defmodule PlugFprof do
+  defmacro __using__(_env) do
+    quote location: :keep do
+      @before_compile PlugFprof
+    end
+  end
+
+  defmacro __before_compile__(_env) do
+    quote location: :keep do
+      defoverridable [call: 2]
+
+      def call(conn, opts) do
+        try do
+          :fprof.trace(:start)
+          super(conn, opts)
+        after
+          :fprof.trace(:stop)
+        end
+      end
+
+    end
+  end
 end
